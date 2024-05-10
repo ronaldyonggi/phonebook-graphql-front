5ec457a93204447ba206dbda527d75e03e207077
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-10 09:32:29 +0800

    implement error handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import { useQuery } from '@apollo/client';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
+import Notify from './components/Notify'
 import { ALL_PERSONS } from './queries';
+import { useState } from 'react';
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS, {
     // pollInterval: 2000
   });
+
+  const notify = message => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000);
+  }
 
   if (result.loading) {
     return <div>loading...</div>;
   }
 
   return (
-    <>
+    <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
-    </>
+      <PersonForm setError={notify} />
+    </div>
   );
 };
 
