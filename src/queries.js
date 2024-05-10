364f6c284d8527aaa40678c992f2e9b83ff363f6
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-10 09:59:22 +0800

    add EDIT_NUMBER query

--- src/queries.js
+++ src/queries.js
@@ -1,4 +1,4 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const ALL_PERSONS = gql`
   query {
@@ -39,6 +39,20 @@ export const FIND_PERSON = gql`
         street
         city
       }
+    }
+  }
+`;
+
+export const EDIT_NUMBER = gql`
+  mutation editNumber($name: String!, $phone: String!) {
+    editNumber(name: $name, phone: $phone) {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
     }
   }
 `;
