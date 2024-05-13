178fe89303f0a61ae2182d7f54b7ecf25368cb94
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-13 15:08:29 +0800

    add logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,52 @@
-import { useQuery } from '@apollo/client';
+import { useApolloClient, useQuery } from '@apollo/client';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
-import Notify from './components/Notify'
+import Notify from './components/Notify';
 import { ALL_PERSONS } from './queries';
 import { useState } from 'react';
 import PhoneForm from './components/PhoneForm';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
-  const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const result = useQuery(ALL_PERSONS, {
     // pollInterval: 2000
   });
+  const client = useApolloClient();
 
-  const notify = message => {
-    setErrorMessage(message)
+  const notify = (message) => {
+    setErrorMessage(message);
     setTimeout(() => {
-      setErrorMessage(null)
+      setErrorMessage(null);
     }, 10000);
-  }
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   if (result.loading) {
     return <div>loading...</div>;
   }
 
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
