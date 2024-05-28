3a25a05b347cef74884e051d3bde4098b9b6f976
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-05-28 22:50:22 +0800

    integrate websocket to client for subscription

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import {
   ApolloProvider,
   InMemoryCache,
   createHttpLink,
-  gql,
+  split,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('phonenumbers-user-token');
@@ -23,26 +26,24 @@ const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
 });
 
+const wsLink = new GraphQLWsLink(createClient({ url: 'ws://localhost:4000' }));
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link: splitLink,
 });
-
-const query = gql`
-  query {
-    allPersons {
-      name
-      phone
-      address {
-        street
-        city
-      }
-      id
-    }
-  }
-`;
-
-client.query({ query }).then((res) => console.log(res.data));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
